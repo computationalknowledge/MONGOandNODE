b8b62865fa1c1f2cccf45024ade9b051890035fa
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-11-24 08:46:59 -0500

    Create deleteMany.js

---
+++ deleteMany.js
@@ -0,0 +1,15 @@
+// see result here:  https://www.screencast.com/t/nEhudFCzyL
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var myquery = { address: /^[A-E]/ };
+  dbo.collection("customers").deleteMany(myquery, function(err, obj) {
+    if (err) throw err;
+    console.log(obj.result.n + " document(s) deleted");
+    console.log(obj.result.n);
+    db.close();
+  });
+});
